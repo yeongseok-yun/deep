09a1da58a954311b3d1d15e8d32d72140939d826
윤영석 <yeongseok.yun@saltlux.com>
2023-08-11 09:13:28 +0900

    matplotlib 파일 추가

--- matplotlibSample.py
+++ matplotlibSample.py
@@ -1,4 +1,22 @@
 import matplotlib.pyplot as plt
+import numpy as np
+#선그래프
+npArr1 = np.array([1,2,3,4,5])
+npArr2 = np.array([1,4,9,16,25])
 
-plt.plot([1,2,3,4,5],[1,4,9,16,25])
+plt.plot(npArr1,npArr2)
+plt.show()
+
+
+
+#산점도 그리기
+plt.scatter(npArr1,npArr2)
+plt.show()
+
+
+
+#넘파이 배열로 산점도 그리기
+x = np.random.randn(1000)
+y = np.random.randn(1000)
+plt.scatter(x,y)
 plt.show()
